d804e6ab4e85a5b7cd00b0dd974ede7cafeb85eb
samfeldman <samantha.ariel.feldman@gmail.com>
2014-11-07 11:02:17 -0500

    Added tconnections to models.rb, deleted signin/signup pages to be put on index.erb, started html in pages and created routes for users, feed and profile

--- models.rb
+++ models.rb
@@ -1,11 +1,17 @@
 class User < ActiveRecord::Base
+	has_many :posts
+	#has_many :links
+
 end
 
 class Post <ActiveRecord::Base
+	belongs_to :user
 end
 
 class Follow <ActiveRecord::Base
+	
 end
 
-class Link <ActiveRecord::Base
-end
+#class Link <ActiveRecord::Base
+#	belongs_to :user
+#end
